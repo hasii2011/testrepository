43ea1b102ec0959511d72d71883ad20d310e3bd1
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-03-08 11:09:31 -0700

    <>[]: <Better versions of these files>
    []
    
    []

--- setup.py
+++ setup.py
@@ -9,37 +9,27 @@ README = (HERE / "README.md").read_text()
 LICENSE = (HERE / 'LICENSE').read_text()
 
 setup(
-    name="ogl",
-    version="0.70.20",
-    author='Humberto A. Sanchez II',
+    name="testrepository",
+    version="1.0.0",
     author_email='Humberto.A.Sanchez.II@gmail.com',
     maintainer='Humberto A. Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
-    description='External Pyut Graphical Shapes',
+    description='For use by versionoverloard',
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/hasii2011/ogl",
-    package_data={
-        'miniogl':              ['py.typed'],
-        'ogl':                  ['py.typed'],
-        'ogl.events':           ['py.typed'],
-        'ogl.preferences':      ['py.typed'],
-        'ogl.resources':        ['py.typed'],
-        'ogl.resources.img':             ['py.typed'],
-        'ogl.resources.img.textdetails': ['py.typed'],
-        'ogl.sd':               ['py.typed'],
-        'ogl.ui':               ['py.typed'],
-        'ogl.ui.valuecontrols': ['py.typed'],
-    },
-
+    url="https://github.com/hasii2011/TestRepository.git",
     packages=[
-        'miniogl',
-        'ogl',
-        'ogl.events',
-        'ogl.preferences',
-        'ogl.resources', 'ogl.resources.img', 'ogl.resources.img.textdetails',
-        'ogl.sd',
-        'ogl.ui', 'ogl.ui.valuecontrols',
+        'testrepo',
     ],
-    install_requires=['Deprecated~=1.2.13', 'pyutmodel~=1.4.0', 'hasiicommon==0.0.7', 'wxPython~=4.2.0'],
+    package_data={
+        'testrepo':          ['py.typed'],
+        'oglio.toXmlV10': ['py.typed']
+    },
+    install_requires=[
+        'wxPython==4.2.0',
+        'hasiicommon~=0.0.7',
+        'pyutmodel~=1.4.0',
+        'ogl~=0.70.20',
+        'untanglepyut~=0.6.40',
+    ],
 )
